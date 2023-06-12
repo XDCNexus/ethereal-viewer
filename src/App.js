71bc99abeb0317c3ae1ffe0d14893a0cc2532c8b
rare <rarenftclub@gmail.com>
2023-06-12 13:30:01 -0700

    add arwes style and sfx

--- src/App.js
+++ src/App.js
@@ -3,20 +3,49 @@ import { useDispatch, useSelector } from "react-redux";
 import { connect } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
-import styled from "styled-components";
+import styled, { keyframes } from 'styled-components';
 import CountDown from "./countdown";
 import Web3 from "web3";
 import { FaTelegramPlane, FaDiscord, FaTwitter } from "react-icons/fa";
 // import mainLogo from './songbird-flare.png';
 import loop from './bg.mp4'
+import { Arwes, ThemeProvider, SoundsProvider, createSounds, createTheme, Frame, Button, Loading, Logo, Words } from 'arwes';
+import clickSound from './object.mp3';
 
 import ScrollTop from "react-scrolltop-button";
 import { nexusPoints } from './NexusPointsList'
 
+const theme = createTheme({
+  typography: {
+    headerFontFamily: 'Orbitron, sans-serif',
+    buttonFontFamily: 'Orbitron, sans-serif',
+  },
+});
+
+const clickAudio = new Audio(clickSound);
+
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const sounds = {
+  shared: { volume: 1 },
+  players: {
+    click: { sound: { src: ['/click.mp3'] } },
+    typing: { sound: { src: ['/type.mp3'] } },
+    deploy: { sound: { src: ['/object.mp3'] } },
+  },
+};
+
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
 
-  export const StyledButton = styled.button`
+export const StyledButton = styled.button`
   padding: 10px;
   border-radius: 10px;
   border: 2px solid;
@@ -90,12 +119,12 @@ export const RoundButtonWrapper = styled.div`
 `;
 
 export const StyledLogo = styled.img`
-  width: 200px;
+  width: 150px;
   @media (min-width: 767px) {
     width: 150px;
   }
-  transition: width 0.5s;
-  transition: height 0.5s;
+  transition: width 0.5s, height 0.5s; /* Combined transition properties */
+  animation: ${rotateAnimation} 30s linear infinite; /* Apply the rotation animation */
 `;
 
 export const StyledImg = styled.img`
@@ -215,69 +244,73 @@ function App() {
   }, [blockchain.account]);
 
   return (
-    <s.Screen>
+    <ThemeProvider theme={theme}>
+      <SoundsProvider sounds={sounds}>
 
-      <ScrollTop
-        text="TOP"
-        distance={100}
-        breakpoint={650}
-        style={{ backgroundColor: "white" }}
-        className="scroll-your-role"
-        speed={500}
-        target={10}
-        icon={<h1>^</h1>}
-      />
+        <Arwes animate>
+          <s.Screen>
 
-      <ResponsiveWrapper>
-        <video
-          autoPlay
-          loop
-          muted
-          style={{
-            position: "fixed",
-            width: "100%",
-            left: "50%",
-            top: "50%",
-            height: "100%",
-            objectFit: "cover",
-            transform: "translate(-50%, -50%)",
-            zIndex: "-1",
+            <ScrollTop
+              text="TOP"
+              distance={100}
+              breakpoint={650}
+              style={{ backgroundColor: "white" }}
+              className="scroll-your-role"
+              speed={500}
+              target={10}
+              icon={<h1>^</h1>}
+            />
 
-
-          }}>
-
-          <source src={loop} type="video/mp4" />
-
-        </video>
-
-        <s.Container
-          flex={1}
-          ai={"center"}
-          style={{ padding: 24, backgroundColor: "var(--primary)", }}
-        >
-          <a rel="noopener noreferrer" href="https://thenexusportal.io/">
-            <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
-          </a>
-
-          <div className="social-container">
-
-          <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/PoweredByNEXUS">
-              <FaTwitter color="white" size={30} />
-            </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/nexusportal">
-              <FaDiscord color="white" size={30} />
-            </a>
+            <ResponsiveWrapper>
+              <video
+                autoPlay
+                loop
+                muted
+                style={{
+                  position: "fixed",
+                  width: "100%",
+                  left: "50%",
+                  top: "50%",
+                  height: "100%",
+                  objectFit: "cover",
+                  transform: "translate(-50%, -50%)",
+                  zIndex: "-1",
 
 
-            {/* <a target="_blank" rel="noopener noreferrer" href="https://t.me/NexusOffical">
+                }}>
+
+                <source src={loop} type="video/mp4" />
+
+              </video>
+
+              <s.Container
+                flex={1}
+                ai={"center"}
+                style={{ padding: 24, backgroundColor: "var(--primary)", }}
+              >
+                <a rel="noopener noreferrer" href="https://thenexusportal.io/">
+                  <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
+                </a>
+
+                <div className="social-container">
+
+                  <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/PoweredByNEXUS">
+                    <FaTwitter color="white" size={30} />
+                  </a>
+                  <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/nexusportal">
+                    <FaDiscord color="white" size={30} />
+                  </a>
+
+
+                  {/* <a target="_blank" rel="noopener noreferrer" href="https://t.me/NexusOffical">
               <FaTelegramPlane color="gold" size={30} />
             </a> */}
 
-          </div>
+                </div>
 
-          <s.Container ai={"center"} jc={"center"}>
+                <s.Container ai={"center"} jc={"center"}>
 
-            {/* <s.TextDescription
+                  {/* <s.TextDescription
               style={{
                 textAlign: "center",
                 color: "pink",
@@ -285,179 +318,195 @@ function App() {
             >
               🚨 If you are having trouble fetching your NFTs on Bifrost please try the other seer app in any other browser like chrome and paste your address in manually.
             </s.TextDescription>
-            <StyledLink target={"_blank"} href={"https://seer2.Nexus.io"}>
+            <StyledLink target={"_blank"} href={"https://seer2.thenexusportal.io"}>
               {"SEER ALTERNATIVE"}
             </StyledLink> */}
-            </s.Container>
-
-
-
-
-          <s.SpacerLarge />
-          <s.Container
-            flex={2}
-
-            style={{
-              backgroundColor: "var(--gold-gradient-box)",
-              padding: 1,
-              // borderRadius: 24,
-              // border: "4px solid var(--secondary)",
-              // boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
-            }}
-          >
-
-            {blockchain.account === "" ||
-              blockchain.smartContract === null ? (
-              <s.Container ai={"center"} jc={"center"}>
-                <s.SpacerSmall />
-                <StyledButton
-                  onClick={(e) => {
-                    e.preventDefault();
-                    dispatch(connect());
-                    getData();
-
-                  }}
-                >
-                  CONNECT
-                </StyledButton>
-
-                {blockchain.errorMsg !== "" ? (
-                  <>
-                    <s.SpacerSmall />
-                    <s.TextDescription
-                      style={{
-                        textAlign: "center",
-                        color: "var(--accent-text)",
-                      }}
-                    >
-                      {blockchain.errorMsg}
-                    </s.TextDescription>
-                  </>
-                ) : null}
-              </s.Container>
-            ) : (
-              <>
-                <s.Container ai={"center"} jc={"center"}>
-
-                  <s.TextDescription
-                    style={{
-                      textAlign: "center",
-                      color: "var(--accent-text)",
-                    }}
-                  >
-
-                    {blockchain.account.length > 0 ? (
-                      String(blockchain.account).substring(0, 6) +
-                      "..." +
-                      String(blockchain.account).substring(36)
-                    ) : (
-                      <span>PLEASE CONNECT</span>
-                    )}
-
-                  </s.TextDescription>
-
-                  <StyledButton
-                    onClick={(e) => {
-                      getWalletTokens();
-                    }}
-                  >
-                    FETCH NFTs
-                  </StyledButton>
-
-                  <s.SpacerSmall />
-
-                  {fetched ? (
-
-
-                    <s.Container ai={"center"} jc={"center"}>
-                      <StyledLink target={"_blank"} href={"https://exrp.minter.thenexusportal.io/"}>
-                        {"MINT"}
-                      </StyledLink>
-
-
-                      <s.TextDescription
-                        style={{
-                          textAlign: "center",
-                          color: "var(--accent-text)",
-                        }}
-                      >
-                        YOU HAVE {nexusBalance} ETHEREALS!
-                      </s.TextDescription>
-
-                      {/* <s.TextDescription
-                        style={{
-                          textAlign: "center",
-                          color: "var(--accent-text)",
-                        }}
-                      >
-                        YOUR TOTAL NEXUS POINTS: {oTotalPoints}
-                      </s.TextDescription> */}
-
-                      {/* <s.TextDescription
-                        style={{
-                          textAlign: "center",
-                          color: "pink",
-                        }}
-                      >
-                        ⚠ Please keep in mind the points displayed here do not include any bonuses!
-                      </s.TextDescription>
-                      <StyledLink target={"_blank"} href={"https://seer2.Nexus.io"}>
-                        {"SEER ALTERNATIVE"}
-                      </StyledLink> */}
-
-                      {/* <s.TextDescription
-                        style={{
-                          textAlign: "center",
-                          color: "var(--accent-text)",
-                        }}
-                      >
-                        YOU HAVE {tPoints} NEXUS POINTS!
-                      </s.TextDescription> */}
-
-
-                      <div>
-                        <div className="container">
-                          <div className="row">
-                            {nexusNfts.map((id) => (
-                              <div key={id} className="col-sm">
-                                <NFTDisplay tokenId={id} nftCount={nexusBalance} points={nexusPoints.at(id - 1)} />
-                              </div>
-                            ))}
-                          </div>
-                        </div>
-                      </div>
-
-                    </s.Container>
-
-                  ) : (
-
-                    <>
-                      <s.TextDescription
-                        style={{
-                          textAlign: "center",
-                          color: "var(--accent-text)",
-                        }}
-                      >
-                        YOU DONT HAVE ANY ETHEREALS!
-                      </s.TextDescription>
-
-                      <StyledLink target={"_blank"} href={"https://exrp.mint.thenexusportal.io/"}>
-                        {"MINT NOW!"}
-                      </StyledLink>
-                    </>
-                  )}
-
                 </s.Container>
 
 
-              </>
-            )}
 
 
-          </s.Container>
-        </s.Container>
-      </ResponsiveWrapper>
-    </s.Screen >
+                <s.SpacerLarge />
+                <s.Container
+                  flex={2}
+
+                  style={{
+                    backgroundColor: "var(--gold-gradient-box)",
+                    padding: 1,
+                    // borderRadius: 24,
+                    // border: "4px solid var(--secondary)",
+                    // boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
+                  }}
+                >
+
+                  {blockchain.account === "" ||
+                    blockchain.smartContract === null ? (
+                    <s.Container ai={"center"} jc={"center"}>
+                      <s.SpacerSmall />
+                      <Frame
+                        animate
+                        level={3}
+                        corners={10}
+                        layer='primary'
+                        onClick={(e) => {
+                          e.preventDefault();
+                          dispatch(connect());
+                          getData();
+                          clickAudio.play();
+                        }}
+                      >
+                        <span style={{ padding: '20px 30px', }}>CONNECT TO THE NEXUS CORE</span>
+
+                      </Frame>
+
+                      {blockchain.errorMsg !== "" ? (
+                        <>
+                          <s.SpacerSmall />
+                          <s.TextDescription
+                            style={{
+                              textAlign: "center",
+                              color: "var(--accent-text)",
+                            }}
+                          >
+                            {blockchain.errorMsg}
+                          </s.TextDescription>
+                        </>
+                      ) : null}
+                    </s.Container>
+                  ) : (
+                    <>
+                      <s.Container ai={"center"} jc={"center"}>
+
+                        <s.TextDescription
+                          style={{
+                            textAlign: "center",
+                            color: "var(--accent-text)",
+                          }}
+                        >
+
+                          {blockchain.account.length > 0 ? (
+                            String(blockchain.account).substring(0, 6) +
+                            "..." +
+                            String(blockchain.account).substring(36)
+                          ) : (
+                            <span>PLEASE CONNECT</span>
+                          )}
+
+                        </s.TextDescription>
+
+
+                        <Frame
+                          animate
+                          level={3}
+                          corners={10}
+                          layer='primary'
+                          onClick={(e) => {
+                            getWalletTokens();
+                            clickAudio.play();
+                          }}
+                        >
+                          <span style={{ padding: '20px 30px', }}>FETCH NFTs</span>
+                        </Frame>
+
+                        <s.SpacerSmall />
+
+                        {fetched ? (
+
+
+                          <s.Container ai={"center"} jc={"center"}>
+                            <StyledLink target={"_blank"} href={"https://exrp.minter.thenexusportal.io/"}>
+                              {"MINT"}
+                            </StyledLink>
+
+
+                            <s.TextDescription
+                              style={{
+                                textAlign: "center",
+                                color: "var(--accent-text)",
+                              }}
+                            >
+                              YOU HAVE {nexusBalance} ETHEREALS!
+                            </s.TextDescription>
+
+                            <s.TextDescription
+                              style={{
+                                textAlign: "center",
+                                color: "var(--accent-text)",
+                              }}
+                            >
+                              YOUR TOTAL NEXUS POINTS: {oTotalPoints}
+                            </s.TextDescription>
+
+                            {/* <s.TextDescription
+                              style={{
+                                textAlign: "center",
+                                color: "pink",
+                              }}
+                            >
+                              ⚠ Please keep in mind the points displayed here do not include any bonuses!
+                            </s.TextDescription> */}
+                            
+                            {/* <StyledLink target={"_blank"} href={"https://seer2.Nexus.io"}>
+                              {"SEER ALTERNATIVE"}
+                            </StyledLink> */}
+
+                            {/* <s.TextDescription
+                              style={{
+                                textAlign: "center",
+                                color: "var(--accent-text)",
+                              }}
+                            >
+                              YOU HAVE {tPoints} NEXUS POINTS!
+                            </s.TextDescription> */}
+
+
+                            <div>
+                              <div className="container">
+                                <div className="row">
+                                  {nexusNfts.map((id) => (
+                                    <div key={id} className="col-sm">
+                                      <NFTDisplay tokenId={id} nftCount={nexusBalance} points={nexusPoints.at(id - 1)} />
+                                    </div>
+                                  ))}
+                                </div>
+                              </div>
+                            </div>
+
+                          </s.Container>
+
+                        ) : (
+
+                          <>
+                            <s.TextDescription
+                              style={{
+                                textAlign: "center",
+                                color: "var(--accent-text)",
+                              }}
+                            >
+                              YOU DONT HAVE ANY ETHEREALS!
+                            </s.TextDescription>
+
+                            <StyledLink target={"_blank"} href={"https://exrp.mint.thenexusportal.io/"}>
+                              {"MINT NOW!"}
+                            </StyledLink>
+                          </>
+                        )}
+
+                      </s.Container>
+
+
+                    </>
+                  )}
+
+
+                </s.Container>
+              </s.Container>
+            </ResponsiveWrapper>
+          </s.Screen >
+        </Arwes>
+      </SoundsProvider>
+
+    </ThemeProvider>
   );
 }
 
@@ -491,7 +540,7 @@ function NFTDisplay({ tokenId, nftCount, points }) {
               color: "var(--accent-text)",
             }}
           >
-            {/* Points: {points} */}
+            Points: {points}
           </s.TextDescription>
           <a href={metadataLink} target="_blank" >
             <img className="card-img-top" src={imageURI} style={{ width: '250px' }} ></img>
